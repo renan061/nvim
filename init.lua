d4906f94cc89b666bc2766c0c9b03cb81cc97543
Renan <renan.061@gmail.com>
2023-05-02 06:20:03 -0300

    Adds golang lsp

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ local lspconfig = require("lspconfig")
 lspconfig.rust_analyzer.setup({on_attach = mappings.on_attach})
 lspconfig.elmls.setup({on_attach = mappings.on_attach})
 lspconfig.hls.setup({on_attach = mappings.on_attach})
+lspconfig.gopls.setup({on_attach = mappings.on_attach})
 
 vim.diagnostic.config({
     virtual_text = false,
