4d269f4cdc6af19d033d1d21afab2570f72bb2ce
Renan Almeida <renan.061@gmail.com>
2023-05-04 16:01:30 -0300

    Updating nvim-tree dependency

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,8 +6,8 @@ local packer = require("packer").startup(function()
     use "Mofiqul/dracula.nvim"
 
     use { -- nvim-tree
-        "kyazdani42/nvim-tree.lua",
-        requires = { "kyazdani42/nvim-web-devicons" },
+        "nvim-tree/nvim-tree.lua",
+        requires = { "nvim-tree/nvim-web-devicons" },
     }
 
     use "whonore/Coqtail"
