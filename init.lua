923a5e2997e38ce387575d54138f1d73034909a9
Renan <renan@anduril.com>
2026-03-22 12:16:45 -0300

    Update configuration

--- init.lua
+++ init.lua
@@ -3,13 +3,16 @@ require("basics")
 require("lsp")
 require("mappings")
 
-local lspconfig = require("lspconfig")
-lspconfig.solidity_ls_nomicfoundation.setup({})
-lspconfig.bashls.setup({})
-lspconfig.ccls.setup({})
-lspconfig.gopls.setup({})
-lspconfig.rust_analyzer.setup({})
-lspconfig.lua_ls.setup({
+-- vim.lsp.enable("solidity_ls_nomicfoundation")
+-- vim.lsp.enable("bashls")
+-- vim.lsp.enable("ccls")
+-- vim.lsp.enable("gopls")
+-- vim.lsp.enable("rust_analyzer")
+-- vim.lsp.config("hls", {})
+-- vim.lsp.config("pyright", {})
+-- vim.lsp.config("elmls", {})
+
+vim.lsp.config("lua_ls", {
     settings = {
         Lua = {
             diagnostics = {
@@ -18,9 +21,7 @@ lspconfig.lua_ls.setup({
         }
     }
 })
--- lspconfig.hls.setup({})
--- lspconfig.pyright.setup({})
--- lspconfig.elmls.setup({})
+vim.lsp.enable("lua_ls")
 
 vim.diagnostic.config({
     virtual_text = false,
