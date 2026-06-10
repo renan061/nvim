3d4ed4f581a2d3d6be0ad703c846195fac3b8a9c
Renan <renan.061@gmail.com>
2026-06-10 19:32:18 -0300

    Update

--- init.lua
+++ init.lua
@@ -3,28 +3,7 @@ require("basics")
 require("lsp")
 require("mappings")
 
--- vim.lsp.enable("solidity_ls_nomicfoundation")
--- vim.lsp.enable("bashls")
--- vim.lsp.enable("ccls")
 vim.lsp.enable("gopls")
--- vim.lsp.enable("rust_analyzer")
--- vim.lsp.config("hls", {})
--- vim.lsp.config("pyright", {})
--- vim.lsp.config("elmls", {})
-
-vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = {
-                enable = false
-            }
-        }
-    }
-})
-
-vim.lsp.config("vtsls", {})
-vim.filetype.add({ extension = { jsx = "javascriptreact" } })
-vim.filetype.add({ extension = { tsx = "typescriptreact" } })
 
 vim.diagnostic.config({
     virtual_text = false,
